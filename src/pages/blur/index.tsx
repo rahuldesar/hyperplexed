9e2c5845adadcf56ca69a9b6ffdd9f070b382d2a
Rahul Desar <desarrahul@gmail.com>
2023-05-14 10:03:00 +0545

    refactor: custom useCursor hook

--- src/pages/blur/index.tsx
+++ src/pages/blur/index.tsx
@@ -1,26 +1,27 @@
-import { useEffect, useState } from "react";
+import useCursorCoordinates from "@/utils/useCursorCoordinates";
+import { useEffect, useRef, useState } from "react";
 
 export default function Somepage() {
-  useEffect(() => {
-    const blob = document.getElementById("blob");
+  const blobRef = useRef<HTMLDivElement>(null);
+  const mouseCoordinates = useCursorCoordinates();
 
-    document.body.onpointermove = (event) => {
-      const { clientX, clientY } = event;
-      if (blob) {
-        blob.animate(
-          {
-            left: `${clientX}px`,
-            top: `${clientY}px`,
-          },
-          { duration: 1250, fill: "forwards" }
-        );
-      }
-    };
-  }, []);
+  const { clientX, clientY } = mouseCoordinates as { clientX: string; clientY: string };
+
+  if (clientX && blobRef.current) {
+    let blob = blobRef.current;
+    blob.animate(
+      {
+        left: `${clientX}px`,
+        top: `${clientY}px`,
+      },
+      { duration: 750, fill: "forwards" }
+    );
+  }
+
   return (
     <div className="relative h-screen overflow-hidden ">
       <div
-        id="blob"
+        ref={blobRef}
         className="translate-middle absolute aspect-square h-36 animate-custom-rotate rounded-full bg-gradient-to-r  from-green-400  to-purple-600 ease-in-out"
       ></div>
       <div id="blur" className="absolute z-10 h-full w-full backdrop-blur-[75px]"></div>
